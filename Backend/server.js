75bebacd80280905c12e6ee879d5c96cfbc788ca
Nicholas Smith <nsmith@ethosgroup.com>
2019-11-12 14:46:55 +0000

    Modified CORS policies (#23)

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,15 @@
 import express from "express";
 import request from "request";
+
 const cors = require('cors');
 
 const bodyParser = require('body-parser');
 const assert = require('assert');
-const app= express.Application = express();
+const app = express.Application = express();
 app.use(bodyParser.json());
 const port = 3000;
 const LATEST_DATA_QUERY = {
-  query: "db.getCollection('Catalog').find({})"
+    query: "db.getCollection('Catalog').find({})"
 };
 
 /**
@@ -23,17 +24,19 @@ const password = 'protectthedough';
 const authMechanism = 'SCRAM-SHA-1';
 
 const NODE_ENV = process.env.NODE_ENV;
-if (NODE_ENV !== 'production') {
-    app.use(cors());
-    url = 'mongodb://localhost:27017';
-}
 var corsOptions;
-else {
+if (NODE_ENV !== 'production') {
+    corsOptions = {
+        origin: false
+    };
+    app.use(cors(corsOptions));
+    url = 'mongodb://localhost:27017';
+} else {
     url = `mongodb://${user}:${password}@db.protectthedough.shop/?authMechanism=${authMechanism}&authSource=ptd`;
     console.log(url);
     corsOptions = {
-	origin: 'https://protectthedough.shop',
-	optionsSuccessStatus: 200
+        origin: 'https://protectthedough.shop',
+        optionsSuccessStatus: 200
     };
     app.use(cors(corsOptions));
 }
@@ -51,43 +54,43 @@ const client = new MongoClient(url, {native_parser: true, useUnifiedTopology: tr
  * CRUD Section
  */
 function handleLatestData(req, res) {
-  request.post(url, (error, _response, body) => {
-    if (body) {
-      let str = JSON.stringify(body);
-      let jsonResult = str.substring(1, str.length - 1);
-      res.contentType("json");
-      res.send(jsonResult);
-    } else {
-      console.log(error);
-      res.sendStatus(500);
-      res.send({ error: "Error connecting to database" });
-    }
-  }).json(LATEST_DATA_QUERY);
+    request.post(url, (error, _response, body) => {
+        if (body) {
+            let str = JSON.stringify(body);
+            let jsonResult = str.substring(1, str.length - 1);
+            res.contentType("json");
+            res.send(jsonResult);
+        } else {
+            console.log(error);
+            res.sendStatus(500);
+            res.send({error: "Error connecting to database"});
+        }
+    }).json(LATEST_DATA_QUERY);
 }
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+    res.send("hello world");
 });
 
 app.get("/catalog", (req, res) => {
-	// User connect method to connect to the Server
-	client.connect(function(err) {
-	    assert.equal(err, null);
-		var db = client.db(dbName);
-		db.collection('catalog').find().toArray(function(err, docs) {
-			if(typeof(docs !== undefined)) {
-				res.send(docs);
-			}
-		});
-	});
+    // User connect method to connect to the Server
+    client.connect(function (err) {
+        assert.equal(err, null);
+        var db = client.db(dbName);
+        db.collection('catalog').find().toArray(function (err, docs) {
+            if (typeof (docs !== undefined)) {
+                res.send(docs);
+            }
+        });
+    });
 });
 
 app.get("/users", (req, res) => {
-    client.connect(function(err) {
+    client.connect(function (err) {
         assert.equal(err, null);
         var db = client.db(dbName);
-        db.collection('users').find().toArray(function(err, docs) {
-            if(typeof(docs) !== undefined) {
+        db.collection('users').find().toArray(function (err, docs) {
+            if (typeof (docs) !== undefined) {
                 res.send(docs);
             }
         })
@@ -95,9 +98,9 @@ app.get("/users", (req, res) => {
 });
 
 app.post("/users", (req, res) => {
-    client.connect(function(err) {
+    client.connect(function (err) {
         var db = client.db(dbName);
-        db.collection('users').insertOne(req.body, function(err, r) {
+        db.collection('users').insertOne(req.body, function (err, r) {
             assert.equal(null, err);
             assert.equal(1, r.insertedCount);
         });
@@ -106,8 +109,8 @@ app.post("/users", (req, res) => {
 });
 
 app.listen(port, err => {
-  if (err) {
-    return console.error(err);
-  }
-  return console.log(`server is listening on port ${port}`);
+    if (err) {
+        return console.error(err);
+    }
+    return console.log(`server is listening on port ${port}`);
 });
